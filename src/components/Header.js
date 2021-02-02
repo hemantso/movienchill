05a0e861a5c5d198aa1fb0186a668eeadba2f967
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-02-02 21:31:06 +0530

    nav element

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
 
 export default class Header extends Component {
-    render() {
-        return (
-            <header></header>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <nav className="navigation">
+          <div className="main-heading" id="name">
+            <h1>
+              Cool<span>Movies</span>
+            </h1>
+          </div>
+          <ul className="topnav" id="nav">
+            <li>
+              Movies
+            </li>
+            <li>
+              Tv Shows
+            </li>
+            <li>
+              <input type="text" placeholder="Search..." />
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
+  }
 }
