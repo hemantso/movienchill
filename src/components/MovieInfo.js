3ac4ce89d7753cadb3433bd81e3f8703845f31ad
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-02-02 21:46:27 +0530

    api and api key

--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
-
+const api = 'https://api.themoviedb.org/3'
+const apiKey = 'a9632aa4c0a084cd40a2f5f911739ec0'
 export default class MovieInfo extends Component {
     render() {
         return (
