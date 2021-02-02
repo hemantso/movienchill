6ed084a0e967b20d649d7285e9e813023b7d004a
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-02-02 21:45:29 +0530

    movie info component

---
+++ src/components/MovieInfo.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+
+export default class MovieInfo extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
