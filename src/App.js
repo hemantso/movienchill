9e583bab634122a4711fac0338c3bcb18e8535a5
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-02-03 19:19:08 +0530

    home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header from "./components/Header"
+import Home from "./components/Home"
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Home />
         <h1>Hemant</h1>
       
       </div>
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Header from './Header'
+export default function Home() {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
