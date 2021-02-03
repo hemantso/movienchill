2b7c6496d86a68d20fd5a874aa146f30b95a6fa1
hemant-soni-vst-au4 <sonihemant4741@gmail.com>
2021-02-03 20:06:19 +0530

    popular movie reducer

---
+++ src/reducers/popularMoviesReducer.js
@@ -0,0 +1,12 @@
+import {
+    FETCH_POPULAR_MOVIES
+  } from '../constant';
+  
+  export default (state = [], action) => {
+    switch (action.type) {
+      case FETCH_POPULAR_MOVIES:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
